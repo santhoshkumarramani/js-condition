98ec953430d96c1b0319764e312e40a97214eb0f
santhoshkumarramani <santhoshkumar37132@gmail.com>
2023-08-05 17:16:00 +0530

    new

---
+++ js/main.js
@@ -0,0 +1,176 @@
+// condition
+
+// 2.Get 5 subject marks from user and find total,average.
+
+
+// let tamil=parseInt(prompt("enter tamil sub mark"));
+// let english=parseInt(prompt("enter english sub mark"));
+// let maths=parseInt(prompt("enter maths sub mark"));
+// let science=parseInt(prompt("enter science sub mark"));
+// let social_science=parseInt(prompt("enter social_science sub mark"));
+
+
+
+// total = (tamil+english+maths+science+social_science);
+// console.log(total)
+
+// if(total){
+	
+	// console.log("Total Marks : "+total);
+	
+	// console.log("Average : "+(total/5));
+	
+// }
+// else{
+	// console.log("enter value numbers only");
+// }
+
+
+
+//3.Write a program to read temperature and display a suitable message according to temperature state below. Temp < 0 then Freezing weather Temp 0-10 then Very Cold weather Temp 10-20 then Cold weather Temp 20-30 then Normal in Temp Temp 30-40 then Its Hot Temp >=40 then Its Very Hot
+
+
+	// let temp = parseInt(prompt("enter temperature value"));
+
+	// if (temp < 0) 
+	  // {
+		// console.log("Freezing weather");
+	  
+	  // } 
+	 // else if (temp >= 0 && temp <= 10) {
+	  
+	  // console.log("Very Cold weather");
+
+	  // }
+	 // else if (temp > 10 && temp <= 20) {
+
+		// console.log("Cold weather");
+
+	  // } 
+	 // else if (temp > 20 && temp <= 30) {
+
+		// console.log("Normal weather");
+
+	  // } 
+	 // else if (temp > 30 && temp <= 40) {
+
+		// console.log("Hot weather");
+
+	  // } 
+	 // else if (temp >= 40) {
+
+		// console.log("Very Hot weather");
+
+	  // } 
+	 // else {
+
+		// console.log("temperature value enter Number");
+
+	  // }
+
+
+
+//4.The given Number is even or odd
+
+
+// let num = parseInt(prompt("enter numbers"));
+
+// if(num%2==0){
+	
+	// console.log("even");
+// }
+// else{
+	
+	// console.log("odd");
+// }
+
+
+//5.Sum of the two given integers. If the two values are same, then returns triple their sum.
+
+
+// let a = parseInt(prompt("enter numbers"));
+// let b = parseInt(prompt("enter numbers"));
+
+// c=a+b;
+
+  // if(a==b){
+	   
+	  // console.log(c**3);
+  // }
+  // else{
+	  
+	  // console.log(c);
+  // }
+  
+  //6.Check two given numbers and return true if one of the number is 50 or if their sum is 50
+  
+  // let a = parseInt(prompt("enter numbers"));
+  // let b = parseInt(prompt("enter numbers"));
+  
+  // if((a+b==50)||(a==50)||(b==50)){
+	  
+	  // console.log("True");
+	  
+  // }
+  // else{
+	  
+	  // console.log("false");
+  // }
+ 
+ 		
+		
+// 7.Check whether a given positive number is a multiple of 3 or a multiple of 7.
+
+	// let num1 = parseInt(prompt("enter numbers"));
+	
+	// if(num1%3==0){
+		
+		// console.log(" multiple of 3 ");
+	// }
+	// else if(num1%7==0){
+		
+		// console.log(" multiple of 7 ");
+	// }
+	// else{
+		
+		// console.log("not multiple");
+	// }
+	
+// 8.Find the largest of three given integers.
+
+	// let a = parseInt(prompt("enter numbers"));
+	// let b = parseInt(prompt("enter numbers"));
+	// let c = parseInt(prompt("enter numbers"));
+	
+	// if(a>b && a>c){
+		
+		// console.log("largest num : "+a);
+	// }
+	// else if(b>a && b>c){
+		
+		// console.log("largest num : "+b);
+	// }
+	// else{
+		
+		// console.log("largest num : "+c);
+	// }
+	
+	
+//9.Find a value which is nearest to 100 from two different given integer values.
+
+	// let a = parseInt(prompt("enter numbers"));
+	// let b = parseInt(prompt("enter numbers"));
+	
+	// let a = 20;
+	// let b = 40;
+	
+	// result_1=100-a;
+	// result_2=100-b;
+	
+	// if(result_1>result_2){
+		// console.log("b");
+	// }
+	// else{
+		// console.log("a");
+	// }
+	
